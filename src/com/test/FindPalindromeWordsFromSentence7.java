91dcc9e51d2382ce6832f242b20ff6301eb20fe5
Dhirendra <yadavdhirendra93@gmail.com>
2024-09-09 00:42:49 -0700

    commit

---
+++ src/com/test/FindPalindromeWordsFromSentence7.java
@@ -0,0 +1,40 @@
+package com.test;
+
+public class FindPalindromeWordsFromSentence7 {
+    public static void main(String[] args) {
+        String input = "My name is nitin and I can speak malayalam";
+        String words[] = input.split(" ");
+        for (String word: words) {
+            if(isPalindrome(word)) {
+
+            }
+
+        }
+
+    }
+
+    public static boolean isPalindrome(String input) {
+        int i = 0;
+        int len = input.length() - 1;
+        while (len > i) {
+            if (input.charAt(i) != input.charAt(len)) {
+                return false;
+            }
+            i--;
+            len++;
+        }
+        return true;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
